49308abdffd53df38a8441cfcfbb254b1ebda346
Alberti Toscano <alberti.toscano1@gmail.com>
2025-02-10 20:16:19 -0700

    Reducción de código js

--- src/pages/contacto/contacto.js
+++ src/pages/contacto/contacto.js
@@ -1,10 +1,50 @@
 
 
+/* // declaración de variables */
+let isValid = true;
+// variables que contienen los ID
+const nombre = "nombre";
+const email = "email";
+const telefono = "telefono";
+const mensaje = "mensaje";
 
+/**
+ * Función que comprueba el contenido de las casillas del formulario y retorna un mensaje en caso de que sea un empty string
+ * @param {* variable de ID} id 
+ */
+
+
+const verificacion = (id) =>{
+    const parametro = document.getElementById(id).value.trim();
+    if (parametro === "") {
+        if (id == "email"){
+            document.getElementById(`error-${id}`).textContent = `Por favor, introduce un email válido.` 
+            isValid = false;
+        }else{
+            document.getElementById(`error-${id}`).textContent = `El ${id} es obligatorio.`
+            isValid = false;
+        }
+    };
+
+}
+
+// mediante la función de verificación, comprueba que el contenido no esté vacio, si cualquier elemento está vacio envia un preventDefault();
 document.getElementById("miFormulario").addEventListener('submit', function(event) {
 
+    verificacion(nombre);
+    verificacion(email);
+    verificacion(telefono);
+    verificacion(mensaje);
 
-const nombre = document.getElementById("nombre").value.trim();
+if (isValid === false) {
+    event.preventDefault();
+
+    console.log(nombre);
+}
+
+});
+
+/* const nombre = document.getElementById("nombre").value.trim();
 if (nombre === "") {
     document.getElementById("errorNombre").textContent = 'El nombre es obligatorio.';
     isValid = false;
@@ -28,12 +68,4 @@ const mensaje = document.getElementById("mensaje").value.trim();
 if (mensaje === "") {
     document.getElementById("errorMensaje").textContent = 'El mensaje es obligatorio.';
     isValid = false;
-}
-
-if (isValid === false) {
-    event.preventDefault();
-
-    console.log(nombre);
-}
-
-});
+} */
