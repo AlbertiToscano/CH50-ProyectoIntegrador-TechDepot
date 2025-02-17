2b4feb899a34554948f29b442d734fac2216c9f7
Alberti Toscano <alberti.toscano1@gmail.com>
2025-02-17 15:49:46 -0700

    Se creo la carpeta de json con dependencias

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// https://vitejs.dev/guide/build.html#multi-page-app
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        aboutUs: resolve(__dirname, 'src/pages/about/about.html'),
+        contact: resolve(__dirname, 'src/pages/contacto/contacto.html'),
+      },
+    },
+  },
+})
