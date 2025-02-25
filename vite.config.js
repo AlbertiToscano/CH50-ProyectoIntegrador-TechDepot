819f9f43267c4842bb401972c3afc4958bafe800
Jorge Espinosa <jorgeusharif@gmail.com>
2025-02-24 20:02:33 -0600

    Fix:Se agrego headear y footer a pagina de admin

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
         aboutUs: resolve(__dirname, 'src/pages/about/about.html'),
         contact: resolve(__dirname, 'src/pages/contacto/contacto.html'),
         terminos: resolve(__dirname, 'src/pages/terminosCondiciones/terminosC.html'),
-        productos: resolve(__dirname, 'src/pages/productos.html')
-        
+        productos: resolve(__dirname, 'src/pages/Productos/productos.html'),
+        pagAdmin: resolve(__dirname, 'src/pages/agregarProductos/agregarP.html')
       },
     },
   },
