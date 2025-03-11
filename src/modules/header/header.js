ae5f48648ff04555fbb5dcf8d9f5f4f7bcba1798
Alberti Toscano <alberti.toscano1@gmail.com>
2025-03-10 18:59:41 -0700

    cambios en construye tu pc

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -31,7 +31,7 @@ const insertHeader = ()=>{
                   <!-- <li><a class="dropdown-item" href="#">Perfil</a></li>
 -->
                   <li><a class="dropdown-item" href="/src/pages/inicioSesion/inicioSesion.html">Iniciar sesion</a></li>
-                  <li><a class="dropdown-item" href="src/pages/registro/registro.html">Registrarse</a></li>
+                  <li><a class="dropdown-item" href="/src/pages/registro/registro.html">Registrarse</a></li>
                   <li><a class="dropdown-item" href="/src/pages/agregarProductos/agregarP.html">Administrador</a></li>
               </ul>
           </div>
